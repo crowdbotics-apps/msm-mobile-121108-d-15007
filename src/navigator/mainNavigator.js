f32c637f0918b69e7d08fa66ea6b3288ea27b4c2
MananSharma <manan@crowdbotics.com>
2020-11-12 12:55:13 +0530

    Update screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -8,7 +8,7 @@ import SideMenu from './sideMenu';
 import BlankScreen014441Navigator from '../features/BlankScreen014441/navigator';
 
 /**
- * new navigators can be imported here
+ * new navigators can be imported to here
  */
 
 const AppNavigator = {
